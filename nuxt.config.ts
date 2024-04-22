3cfbc896b6fd2b2176b512b44ad9d406ca8e68c6
francjpd <francjpd@gmail.com>
2024-04-22 21:43:32 +0200

    chore: added tailwind and head info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Book wander',
+      titleTemplate: 'Book wander',
+      meta: [
+        { name: 'description', content: 'Book your travel and travel with ease 🗺️' },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/logo.svg'
+        }
+      ]
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss']
 })
