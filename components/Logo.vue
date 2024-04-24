ad6ae040eec1f22c7e83e3a1cf3032dbc3cbd157
francjpd <francjpd@gmail.com>
2024-04-24 19:13:46 +0200

    feat: logo added

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <div class="inline-flex">
+            <img src="/logo.svg" class="h-14" />
+            <div class="flex flex-col">
+                <div class="font-semibold text-fuzzy-wuzzy-brown-600 text-3xl m-0">
+                    Book
+                </div>
+                <div class="dark:text-lightning-yellow-500 text-2xl leading-3 m-0">
+                    wander
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
