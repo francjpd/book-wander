3af6d62018f31ddf905e8c5291a1eddf2ccbc467
francjpd <fpoleo@veridas.com>
2024-05-01 12:00:05 +0200

    feat(DataTableImage): now is able to click on it to see it on bigger size

--- components/travels/DataTableImage.vue
+++ components/travels/DataTableImage.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
-import { enUS } from 'date-fns/locale'
-import { formatWithOptions } from 'date-fns/fp'
 import type { Travel } from '~/types/travel';
 
 const props = defineProps<{
     travel: Travel
 }>()
 
-const dayFormat = formatWithOptions({ locale: enUS }, 'dd')
-const monthFormat = formatWithOptions({ locale: enUS }, 'MMM')
-
-
 </script>
 <template>
     <div>
-        <div class="flex flex-col items-center md:h-36 md:w-56">
-            <img :src="travel.picture" class="object-cover h-full w-12/12 rounded-md " alt="Destiny's picture" />
-        </div>
+
+        <Dialog>
+            <DialogTrigger>
+                <div class="flex flex-col items-center md:h-36 md:w-56">
+                    <img :src="travel.picture" class="object-cover h-full w-12/12 rounded-md "
+                        :alt="`${travel.name}'s thumbnail picture`" />
+                </div>
+            </DialogTrigger>
+            <DialogContent>
+                <img :src="travel.picture" class="object-cover h-full w-12/12 rounded-md "
+                :alt="`${travel.name}'s full size picture`" />
+            </DialogContent>
+        </Dialog>
     </div>
 </template>
